a34a24a3526f7c9b05c3d1518729f8f7945e3adf
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2025-08-12 15:14:53 +0100

    chore: refactor BigNumNativeState

--- cpp/NativeCslMobileBridgeModule.cpp
+++ cpp/NativeCslMobileBridgeModule.cpp
@@ -1,261 +1,216 @@
 #include "NativeCslMobileBridgeModule.h"
-#include <jsi/jsi.h>
 #include "react_native_haskell_shelley.h"
+#include <jsi/jsi.h>
+#include <memory>
+#include <unordered_map>
+#include <string>
 
 using namespace facebook::jsi;
 using namespace std;
 
 namespace cslmobilebridge {
 
-class BigNumNativeState : public NativeState {
-public:
-  BigNumNativeState(RPtr bigNumPtr) : bigNumPtr_(bigNumPtr) {}
-
-  ~BigNumNativeState() {
-    csl_bridge_rptr_free(&bigNumPtr_);
-  }
-
-  RPtr getBigNumPtr() const { return bigNumPtr_; }
-
-private:
-  RPtr bigNumPtr_;
+// ===== RAII Wrappers =====
+struct ScopedCharPtr {
+  CharPtr ptr = nullptr;
+  ~ScopedCharPtr() { if (ptr) csl_bridge_charptr_free(&ptr); }
 };
 
-static Object createBigNumObject(Runtime& runtime, const RPtr& bigNumPtr) {
-  Object obj(runtime);
-  auto nativeState = std::make_shared<BigNumNativeState>(bigNumPtr);
+struct ScopedDataPtr {
+  DataPtr ptr{nullptr, 0};
+  ~ScopedDataPtr() { if (ptr.ptr) csl_bridge_dataptr_free(&ptr); }
+};
+
+struct ScopedErrorPtr {
+  CharPtr ptr = nullptr;
+  ~ScopedErrorPtr() { if (ptr) csl_bridge_charptr_free(&ptr); }
+};
+
+// ===== NativeState for BigNum =====
+class BigNumNativeState : public NativeState {
+public:
+  explicit BigNumNativeState(RPtr ptr) : ptr_(ptr) {}
+  ~BigNumNativeState() { if (ptr_._0) csl_bridge_rptr_free(&ptr_); }
+  [[nodiscard]] RPtr get() const { return ptr_; }
+private:
+  RPtr ptr_{nullptr};
+};
+
+// ===== Helpers =====
+static Object createBigNum(Runtime& runtime, const RPtr& ptr) {
+  auto obj = Object(runtime);
+  auto nativeState = make_shared<BigNumNativeState>(ptr);
   obj.setNativeState(runtime, nativeState);
   return obj;
 }
 
-static BigNumNativeState& getBigNumNativeState(Runtime& runtime, const Object& obj) {
-  if (!obj.hasNativeState<BigNumNativeState>(runtime)) {
-    throw JSError(runtime, "Object is not a BigNum");
-  }
-  auto nativeState = obj.getNativeState<BigNumNativeState>(runtime);
-  return *nativeState;
+static const BigNumNativeState& getBigNumState(Runtime& runtime, const Value& val, const char* argName) {
+  if (!val.isObject()) throw JSError(runtime, string("Expected BigNum for ") + argName);
+  auto obj = val.asObject(runtime);
+  if (!obj.hasNativeState<BigNumNativeState>(runtime))
+    throw JSError(runtime, string("Object is not a BigNum: ") + argName);
+  return *obj.getNativeState<BigNumNativeState>(runtime);
 }
 
-static Value bigNumToStr(Runtime& runtime, const Object& thisObj) {
-  auto& nativeState = getBigNumNativeState(runtime, thisObj);
-  CharPtr result;
-  CharPtr error;
-  if (!csl_bridge_big_num_to_str(nativeState.getBigNumPtr(), &result, &error)) {
-    throw JSError(runtime, string(error));
+// Generic error-handling wrapper for CSL calls returning bool
+template<typename Fn>
+static void callCsl(Runtime& runtime, Fn fn) {
+  ScopedErrorPtr err;
+  if (!fn(&err.ptr)) {
+    throw JSError(runtime, err.ptr ? err.ptr : "unknown error");
   }
-  auto jsString = String::createFromUtf8(runtime, string(result));
-  csl_bridge_charptr_free(&result);
-  return jsString;
 }
 
-static Value bigNumCheckedAdd(Runtime& runtime, const Object& thisObj, const Value& other) {
-  if (!other.isObject()) {
-    throw JSError(runtime, "BigNum.checked_add: Argument must be a BigNum");
-  }
-  
-  auto& nativeState = getBigNumNativeState(runtime, thisObj);
-  auto& otherNativeState = getBigNumNativeState(runtime, other.asObject(runtime));
-  
+// Generic wrapper for CSL calls returning a BigNum (RPtr)
+template<typename Fn>
+static Value callCslBigNum(Runtime& runtime, Fn fn) {
   RPtr result;
-  CharPtr error;
-  if (!csl_bridge_big_num_checked_add(nativeState.getBigNumPtr(), otherNativeState.getBigNumPtr(), &result, &error)) {
-    throw JSError(runtime, string(error));
-  }
-  return createBigNumObject(runtime, result);
+  callCsl(runtime, [&](CharPtr* e) { return fn(&result, e); });
+  return createBigNum(runtime, result);
 }
 
-static Value bigNumCheckedSub(Runtime& runtime, const Object& thisObj, const Value& other) {
-  if (!other.isObject()) {
-    throw JSError(runtime, "BigNum.checked_sub: Argument must be a BigNum");
-  }
-  
-  auto& nativeState = getBigNumNativeState(runtime, thisObj);
-  auto& otherNativeState = getBigNumNativeState(runtime, other.asObject(runtime));
-  
-  RPtr result;
-  CharPtr error;
-  if (!csl_bridge_big_num_checked_sub(nativeState.getBigNumPtr(), otherNativeState.getBigNumPtr(), &result, &error)) {
-    throw JSError(runtime, string(error));
-  }
-  return createBigNumObject(runtime, result);
-}
+// ===== HostObject =====
+class CslMobileBridge : public HostObject {
+public:
+  explicit CslMobileBridge(Runtime& rt) {
+    using FN = function<Value(Runtime&, const Value*, size_t)>;
+    auto makeFn = [&](const char* name, int arity, FN fn) {
+      methods_.emplace(name,
+        Value(rt, Function::createFromHostFunction(rt, PropNameID::forUtf8(rt, name), arity,
+          [fn](Runtime& runtime, const Value&, const Value* args, size_t count) { return fn(runtime, args, count); }
+        ))
+      );
+    };
 
-static Value bigNumClampedSub(Runtime& runtime, const Object& thisObj, const Value& other) {
-  if (!other.isObject()) {
-    throw JSError(runtime, "BigNum.clamped_sub: Argument must be a BigNum");
-  }
-  
-  auto& nativeState = getBigNumNativeState(runtime, thisObj);
-  auto& otherNativeState = getBigNumNativeState(runtime, other.asObject(runtime));
-  
-  RPtr result;
-  CharPtr error;
-  if (!csl_bridge_big_num_clamped_sub(nativeState.getBigNumPtr(), otherNativeState.getBigNumPtr(), &result, &error)) {
-    throw JSError(runtime, string(error));
-  }
-  return createBigNumObject(runtime, result);
-}
-
-static Value bigNumCompare(Runtime& runtime, const Object& thisObj, const Value& other) {
-  if (!other.isObject()) {
-    throw JSError(runtime, "BigNum.compare: Argument must be a BigNum");
-  }
-  
-  auto& nativeState = getBigNumNativeState(runtime, thisObj);
-  auto& otherNativeState = getBigNumNativeState(runtime, other.asObject(runtime));
-  
-  int64_t result;
-  CharPtr error;
-  if (!csl_bridge_big_num_compare(nativeState.getBigNumPtr(), otherNativeState.getBigNumPtr(), &result, &error)) {
-    throw JSError(runtime, string(error));
-  }
-  return Value((int)result);
-}
-
-void installCslMobileBridge(Runtime& runtime) {
-  // Register BigNum constructor
-  auto bigNumConstructor = Function::createFromHostFunction(
-    runtime,
-    PropNameID::forAscii(runtime, "BigNum"),
-    1,
-    [](Runtime& runtime, const Value& thisValue, const Value* arguments, size_t count) -> Value {
-      if (count == 0) {
-        throw JSError(runtime, "BigNum constructor requires arguments");
-      }
-      
-      if (!arguments[0].isString()) {
-        throw JSError(runtime, "BigNum constructor requires a string argument");
-      }
-      
-      string str = arguments[0].getString(runtime).utf8(runtime);
+    // ==== BigNum methods ====
+    makeFn("bigNumFromStr", 1, [](Runtime& rt, const Value* args, size_t) {
+      auto str = args[0].asString(rt).utf8(rt);
       RPtr result;
-      CharPtr error;
-      if (!csl_bridge_big_num_from_str(str.c_str(), &result, &error)) {
-        throw JSError(runtime, string(error));
+      callCsl(rt, [&](CharPtr* e) { return csl_bridge_big_num_from_str(str.c_str(), &result, e); });
+      return createBigNum(rt, result);
+    });
+
+    makeFn("bigNumToStr", 1, [](Runtime& rt, const Value* args, size_t) {
+      auto& a = getBigNumState(rt, args[0], "bigNumToStr.arg0");
+      ScopedCharPtr out;
+      callCsl(rt, [&](CharPtr* e) { return csl_bridge_big_num_to_str(a.get(), &out.ptr, e); });
+      return String::createFromUtf8(rt, out.ptr);
+    });
+
+    makeFn("bigNumCheckedAdd", 2, [](Runtime& rt, const Value* args, size_t) {
+      auto& a = getBigNumState(rt, args[0], "a");
+      auto& b = getBigNumState(rt, args[1], "b");
+      return callCslBigNum(rt, [&](RPtr* r, CharPtr* e) { return csl_bridge_big_num_checked_add(a.get(), b.get(), r, e); });
+    });
+
+    makeFn("bigNumCheckedSub", 2, [](Runtime& rt, const Value* args, size_t) {
+        auto& a = getBigNumState(rt, args[0], "a");
+        auto& b = getBigNumState(rt, args[1], "b");
+        return callCslBigNum(rt, [&](RPtr* r, CharPtr* e) { return csl_bridge_big_num_checked_sub(a.get(), b.get(), r, e); });
+    });
+
+    makeFn("bigNumClampedSub", 2, [](Runtime& rt, const Value* args, size_t) {
+        auto& a = getBigNumState(rt, args[0], "a");
+        auto& b = getBigNumState(rt, args[1], "b");
+        return callCslBigNum(rt, [&](RPtr* r, CharPtr* e) { return csl_bridge_big_num_clamped_sub(a.get(), b.get(), r, e); });
+    });
+
+    makeFn("bigNumCompare", 2, [](Runtime& rt, const Value* args, size_t) {
+        auto& a = getBigNumState(rt, args[0], "a");
+        auto& b = getBigNumState(rt, args[1], "b");
+        int64_t result;
+        callCsl(rt, [&](CharPtr* e) { return csl_bridge_big_num_compare(a.get(), b.get(), &result, e); });
+        return Value((int)result);
+    });
+
+    makeFn("bigNumIsZero", 1, [](Runtime& rt, const Value* args, size_t) {
+        auto& a = getBigNumState(rt, args[0], "a");
+        bool result;
+        callCsl(rt, [&](CharPtr* e) { return csl_bridge_big_num_is_zero(a.get(), &result, e); });
+        return Value(result);
+    });
+
+    makeFn("bigNumCheckedMul", 2, [](Runtime& rt, const Value* args, size_t) {
+        auto& a = getBigNumState(rt, args[0], "a");
+        auto& b = getBigNumState(rt, args[1], "b");
+        return callCslBigNum(rt, [&](RPtr* r, CharPtr* e) { return csl_bridge_big_num_checked_mul(a.get(), b.get(), r, e); });
+    });
+
+    makeFn("bigNumDivFloor", 2, [](Runtime& rt, const Value* args, size_t) {
+        auto& a = getBigNumState(rt, args[0], "a");
+        auto& b = getBigNumState(rt, args[1], "b");
+        return callCslBigNum(rt, [&](RPtr* r, CharPtr* e) { return csl_bridge_big_num_div_floor(a.get(), b.get(), r, e); });
+    });
+
+    makeFn("bigNumLessThan", 2, [](Runtime& rt, const Value* args, size_t) {
+        auto& a = getBigNumState(rt, args[0], "a");
+        auto& b = getBigNumState(rt, args[1], "b");
+        bool result;
+        callCsl(rt, [&](CharPtr* e) { return csl_bridge_big_num_less_than(a.get(), b.get(), &result, e); });
+        return Value(result);
+    });
+
+    makeFn("bigNumToHex", 1, [](Runtime& rt, const Value* args, size_t) {
+        auto& a = getBigNumState(rt, args[0], "a");
+        ScopedCharPtr out;
+        callCsl(rt, [&](CharPtr* e) { return csl_bridge_big_num_to_hex(a.get(), &out.ptr, e); });
+        return String::createFromUtf8(rt, out.ptr);
+    });
+
+    makeFn("bigNumToJson", 1, [](Runtime& rt, const Value* args, size_t) {
+        auto& a = getBigNumState(rt, args[0], "a");
+        ScopedCharPtr out;
+        callCsl(rt, [&](CharPtr* e) { return csl_bridge_big_num_to_json(a.get(), &out.ptr, e); });
+        return String::createFromUtf8(rt, out.ptr);
+    });
+
+    makeFn("bigNumFromBytes", 1, [](Runtime& rt, const Value* args, size_t) {
+      auto array = args[0].asObject(rt).asArray(rt);
+      auto size = array.size(rt);
+      std::vector<uint8_t> bytes(size);
+      for (size_t i = 0; i < size; ++i) {
+        bytes[i] = static_cast<uint8_t>(array.getValueAtIndex(rt, i).asNumber());
       }
-      return createBigNumObject(runtime, result);
-    }
-  );
-  
-  // Add prototype methods
-  auto prototype = Object(runtime);
-  
-  prototype.setProperty(
-    runtime,
-    "to_str",
-    Function::createFromHostFunction(
-      runtime,
-      PropNameID::forAscii(runtime, "to_str"),
-      0,
-      [](Runtime& runtime, const Value& thisValue, const Value* arguments, size_t count) -> Value {
-        if (!thisValue.isObject()) {
-          throw JSError(runtime, "BigNum.to_str must be called on a BigNum object");
-        }
-        return bigNumToStr(runtime, thisValue.asObject(runtime));
-      }
-    )
-  );
-  
-  prototype.setProperty(
-    runtime,
-    "checked_add",
-    Function::createFromHostFunction(
-      runtime,
-      PropNameID::forAscii(runtime, "checked_add"),
-      1,
-      [](Runtime& runtime, const Value& thisValue, const Value* arguments, size_t count) -> Value {
-        if (!thisValue.isObject()) {
-          throw JSError(runtime, "BigNum.checked_add must be called on a BigNum object");
-        }
-        if (count < 1) {
-          throw JSError(runtime, "BigNum.checked_add requires one argument");
-        }
-        return bigNumCheckedAdd(runtime, thisValue.asObject(runtime), arguments[0]);
-      }
-    )
-  );
-  
-  prototype.setProperty(
-    runtime,
-    "checked_sub",
-    Function::createFromHostFunction(
-      runtime,
-      PropNameID::forAscii(runtime, "checked_sub"),
-      1,
-      [](Runtime& runtime, const Value& thisValue, const Value* arguments, size_t count) -> Value {
-        if (!thisValue.isObject()) {
-          throw JSError(runtime, "BigNum.checked_sub must be called on a BigNum object");
-        }
-        if (count < 1) {
-          throw JSError(runtime, "BigNum.checked_sub requires one argument");
-        }
-        return bigNumCheckedSub(runtime, thisValue.asObject(runtime), arguments[0]);
-      }
-    )
-  );
-  
-  prototype.setProperty(
-    runtime,
-    "clamped_sub",
-    Function::createFromHostFunction(
-      runtime,
-      PropNameID::forAscii(runtime, "clamped_sub"),
-      1,
-      [](Runtime& runtime, const Value& thisValue, const Value* arguments, size_t count) -> Value {
-        if (!thisValue.isObject()) {
-          throw JSError(runtime, "BigNum.clamped_sub must be called on a BigNum object");
-        }
-        if (count < 1) {
-          throw JSError(runtime, "BigNum.clamped_sub requires one argument");
-        }
-        return bigNumClampedSub(runtime, thisValue.asObject(runtime), arguments[0]);
-      }
-    )
-  );
-  
-  prototype.setProperty(
-    runtime,
-    "compare",
-    Function::createFromHostFunction(
-      runtime,
-      PropNameID::forAscii(runtime, "compare"),
-      1,
-      [](Runtime& runtime, const Value& thisValue, const Value* arguments, size_t count) -> Value {
-        if (!thisValue.isObject()) {
-          throw JSError(runtime, "BigNum.compare must be called on a BigNum object");
-        }
-        if (count < 1) {
-          throw JSError(runtime, "BigNum.compare requires one argument");
-        }
-        return bigNumCompare(runtime, thisValue.asObject(runtime), arguments[0]);
-      }
-    )
-  );
-  
-  bigNumConstructor.setProperty(runtime, "prototype", prototype);
-  
-  // Add static from_str method
-  auto fromStr = Function::createFromHostFunction(
-    runtime,
-    PropNameID::forAscii(runtime, "from_str"),
-    1,
-    [](Runtime& runtime, const Value& thisValue, const Value* arguments, size_t count) -> Value {
-      if (count == 0 || !arguments[0].isString()) {
-        throw JSError(runtime, "BigNum.from_str requires a string argument");
-      }
-      
-      string str = arguments[0].getString(runtime).utf8(runtime);
       RPtr result;
-      CharPtr error;
-      if (!csl_bridge_big_num_from_str(str.c_str(), &result, &error)) {
-        throw JSError(runtime, string(error));
+      callCsl(rt, [&](CharPtr* e) { return csl_bridge_big_num_from_bytes(bytes.data(), size, &result, e); });
+      return createBigNum(rt, result);
+    });
+
+    makeFn("bigNumToBytes", 1, [](Runtime& rt, const Value* args, size_t) {
+      auto& a = getBigNumState(rt, args[0], "a");
+      ScopedDataPtr out;
+      callCsl(rt, [&](CharPtr* e) { return csl_bridge_big_num_to_bytes(a.get(), &out.ptr, e); });
+      auto array = Array(rt, out.ptr.len);
+      for (size_t i = 0; i < out.ptr.len; ++i) {
+        array.setValueAtIndex(rt, i, Value(static_cast<double>(out.ptr.ptr[i])));
       }
-      return createBigNumObject(runtime, result);
-    }
-  );
-  
-  bigNumConstructor.setProperty(runtime, "from_str", move(fromStr));
-  runtime.global().setProperty(runtime, "BigNum", move(bigNumConstructor));
-}
+      return array;
+    });
+
+    // Constants
+    makeFn("bigNumZero", 0, [](Runtime& rt, const Value*, size_t) {
+      return callCslBigNum(rt, csl_bridge_big_num_zero);
+    });
+    makeFn("bigNumOne", 0, [](Runtime& rt, const Value*, size_t) {
+      return callCslBigNum(rt, csl_bridge_big_num_one);
+    });
+    makeFn("bigNumMaxValue", 0, [](Runtime& rt, const Value*, size_t) {
+      return callCslBigNum(rt, csl_bridge_big_num_max_value);
+    });
+  }
+
+  Value get(Runtime& rt, const PropNameID& name) override {
+    auto it = methods_.find(name.utf8(rt));
+    return it != methods_.end() ? Value(rt, it->second) : Value::undefined();
+  }
+
+  static void install(Runtime& rt) {
+    rt.global().setProperty(rt, "CslMobileBridge",
+      Object::createFromHostObject(rt, make_shared<CslMobileBridge>(rt)));
+  }
+
+private:
+  unordered_map<string, Value> methods_;
+};
 
 } // namespace cslmobilebridge
--- cpp/NativeCslMobileBridgeModule.h
+++ cpp/NativeCslMobileBridgeModule.h
@@ -1,9 +1,22 @@
 #pragma once
 
 #include <jsi/jsi.h>
+#include <string>
+#include <unordered_map>
 
 namespace cslmobilebridge {
 
-void installCslMobileBridge(facebook::jsi::Runtime& runtime);
+class JSI_EXPORT CslMobileBridge : public facebook::jsi::HostObject {
+public:
+  CslMobileBridge(facebook::jsi::Runtime& rt);
+
+  static void install(facebook::jsi::Runtime& rt);
+
+  facebook::jsi::Value get(facebook::jsi::Runtime&, const facebook::jsi::PropNameID& name) override;
+
+private:
+  facebook::jsi::Runtime& runtime_;
+  std::unordered_map<std::string, facebook::jsi::Value> methods_;
+};
 
 } // namespace cslmobilebridge
