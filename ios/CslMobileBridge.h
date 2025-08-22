ddc28f007e22a50a484eea5b048bf0054c4257e2
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2025-08-22 16:51:38 +0100

    update the bridge

--- ios/CslMobileBridge.h
+++ ios/CslMobileBridge.h
@@ -1,5 +1,9 @@
 #import <CslMobileBridgeSpec/CslMobileBridgeSpec.h>
+#import <ReactCommon/RCTTurboModule.h>
 
-@interface CslMobileBridge : NSObject <NativeCslMobileBridgeSpec>
+@interface CslMobileBridge : NSObject <NativeCslMobileBridgeSpec, RCTTurboModule>
+
+- (std::shared_ptr<facebook::react::TurboModule>)
+    getTurboModule:(const facebook::react::ObjCTurboModule::InitParams &)params;
 
 @end
