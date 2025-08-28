78ae8419833cc7e9b5423c1886dfcdc40fd4855f
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2025-08-28 16:16:04 +0100

    it works

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,18 +1,51 @@
-const path = require("path");
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+const escape = require('escape-string-regexp');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const pak = require('../package.json');
 
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, "..");
+const root = path.resolve(__dirname, '..');
+const modules = Object.keys({ ...pak.peerDependencies });
 
-const config = getDefaultConfig(projectRoot);
+// Path to the library at the repo root
+const cslMobileBridgePath = root; // since lib is at repo root
 
-config.watchFolders = [workspaceRoot];
+const defaultConfig = getDefaultConfig(__dirname);
 
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, "node_modules"),
-  path.resolve(workspaceRoot, "node_modules"),
-];
+module.exports = {
+  ...defaultConfig,
 
-config.resolver.disableHierarchicalLookup = true;
+  // 👀 watch the root too, so changes in src/ are picked up
+  watchFolders: [root],
 
-module.exports = config;
+  resolver: {
+    ...defaultConfig.resolver,
+
+    // keep deduping peer deps
+    blacklistRE: exclusionList(
+      modules.map(
+        (m) =>
+          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+      )
+    ),
+
+    // 👀 map the package name to the root
+    extraNodeModules: {
+      ...modules.reduce((acc, name) => {
+        acc[name] = path.join(__dirname, 'node_modules', name);
+        return acc;
+      }, {}),
+      '@emurgo/csl-mobile-bridge-jsi': cslMobileBridgePath,
+    },
+  },
+
+  transformer: {
+    ...defaultConfig.transformer,
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
